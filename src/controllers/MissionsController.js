a3ee419e4471beef554f453bc4723c924fdbef90
heather bickish <hbickish@gmail.com>
2024-11-20 16:15:04 -0700

    creating missions

--- src/controllers/MissionsController.js
+++ src/controllers/MissionsController.js
@@ -6,12 +6,23 @@ export class MissionsController extends BaseController {
     super('api/missions')
     this.router
       .get('', this.getAllMissions)
+      .post('', this.createMission)
   }
 
 
   async getAllMissions(request, response, next) {
     try {
       const missions = await missionsService.getAllMissions()
+      response.send(missions)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async createMission(request, response, next) {
+    try {
+      const missionData = request.body
+      const missions = await missionsService.createMission(missionData)
       response.send(missions)
     } catch (error) {
       next(error)
--- src/services/MissionsService.js
+++ src/services/MissionsService.js
@@ -1,6 +1,12 @@
 import { dbContext } from "../db/DbContext"
 
 class MissionsService {
+  async createMission(missionData) {
+    const mission = await dbContext.Missions.create(missionData)
+    await mission.populate('rat', '-name -picture')
+    await mission.populate('location')
+    return mission
+  }
   async getAllMissions() {
     const mission = await dbContext.Missions.find().populate('location').populate('rat', '-name -picture')
     return mission
